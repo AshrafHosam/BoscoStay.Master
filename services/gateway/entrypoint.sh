fd98ac59ee581603a43291d4dda80e814f780950
AshrafHosam <ashraf.hosam.ibrahim@gmail.com>
2025-12-30 23:01:48 +0200

    - api gateway and docker compose adjustments

---
+++ services/gateway/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cat > /etc/nginx/nginx.conf <<EOF
+events { }
+
+http {
+    server {
+        listen 80;
+
+        location /booking/ {
+            proxy_pass http://booking-node:3000;
+        }
+
+        location /apartments/ {
+            proxy_pass http://apartments-dotnet:8080/;
+        }
+
+        location /search/ {
+            proxy_pass http://java-service:8080;
+        }
+    }
+}
+EOF
+
+# Start nginx in foreground
+nginx -g "daemon off;"
